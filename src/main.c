1cc41eb308ecb1423747e7a495ba70fcfa80e4fd
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-05-17 18:14:09 -0500

    support for key events

--- src/main.c
+++ src/main.c
@@ -3,6 +3,15 @@
 
 #include<SDL2/SDL.h>
 
+enum KeyPressSurfaces {
+	KEY_PRESS_SURFACE_DEFAULT,
+    KEY_PRESS_SURFACE_UP,
+    KEY_PRESS_SURFACE_DOWN,
+    KEY_PRESS_SURFACE_LEFT,
+    KEY_PRESS_SURFACE_RIGHT,
+    KEY_PRESS_SURFACE_TOTAL
+};
+
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
@@ -15,11 +24,17 @@ bool load_media();
 // free media and close SDL
 void free_media();
 
+SDL_Surface* load_surface(const char* path);
+
 SDL_Window* global_window = NULL;
 
 SDL_Surface* global_screen_surface = NULL;
 
 SDL_Surface* image = NULL;
+
+SDL_Surface* global_keypress_surfaces[KEY_PRESS_SURFACE_TOTAL];
+
+SDL_Surface* global_current_surface = NULL;
 
 int main(int argc, char* args[]) {
 	if (!init()) {
@@ -103,7 +118,15 @@ void free_media() {
 }
 
 
+SDL_Surface* load_surface(const char* path) {
+	SDL_Surface* loaded_surface = SDL_LoadBMP(path);
 
+	if (loaded_surface == NULL) {
+		printf("Unable to load image %s! SDL Error: %s\n", path, SDL_GetError());
+	}
+
+	return loaded_surface;
+}
 
 
 
