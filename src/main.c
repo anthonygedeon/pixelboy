a7e3668bc359c732857562177cb1a8d8e70c7f33
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-05-18 13:04:52 -0500

    add headers

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#include<SDL2/SDL.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
 
 enum KeyPressSurfaces {
 	KEY_PRESS_SURFACE_DEFAULT,
