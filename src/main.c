53948fff8c49f21364e6ac025f33388779762ad4
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-05-21 23:07:15 -0500

    increase window size

--- src/main.c
+++ src/main.c
@@ -6,8 +6,9 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
 
-const int SCREEN_WIDTH = 640;
+const int SCREEN_WIDTH = 940;
 const int SCREEN_HEIGHT = 480;
+const int FPS = 60;
 
 // loads media contents
 bool load_media();
